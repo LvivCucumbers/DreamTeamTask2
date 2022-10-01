a2d3b89c3d3e4afeefd39360d5dd29a087a44feb
= <fergo.rossii@gmail.com>
2022-10-01 16:22:40 +0300

    Renamed component

--- app/src/App.js
+++ app/src/App.js
@@ -1,12 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
-import StarWars from './pages/StarWars';
+import Home from './pages/Home';
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<StarWars />} />
+      <Route path="/" element={<Home />} />
     </Routes>
   );
 }
