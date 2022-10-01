2d225e0aa63642ee297c13341901adfb9602e191
= <fergo.rossii@gmail.com>
2022-10-01 16:20:16 +0300

    Created main page

---
+++ app/src/pages/StarWars.js
@@ -0,0 +1,23 @@
+export default function StarWars() {
+  return (
+    <>
+      <div>
+        <img src='./unknow_actor.jpg' width="200" />
+        <b className='title'>Name</b>
+        <div className='infoList'>
+          <p>Gender:</p>
+          <p>Male</p>
+        </div>
+        <div className='infoList'>
+          <p>Birth Day:</p>
+          <p>01/02/1976</p>
+        </div>
+        <div className='infoList'>
+          <p>Eye color:</p>
+          <p>Red</p>
+        </div>
+        <button>Next</button>
+      </div>
+    </>
+  );
+}
